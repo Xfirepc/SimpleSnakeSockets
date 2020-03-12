200d7fde8c6af1b4f4550a9c77b89e8d0ba8eed4
XfirePc <vinygfx@gmail.com>
2020-03-11 20:18:50 -0500

    add client server

--- client-server/simple-client.py
+++ client-server/simple-client.py
@@ -1,13 +1,13 @@
+#!/usr/bin/env python3
+
 import socket
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 65432        # The port used by the server
 
-client.connect(('0.0.0.0', 8080))
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
 
-client.send("I am CLIENT<br>")
-
-from_server = client.recv(4096)
-
-client.close()
-
-print from_server
+print('Received', repr(data))
--- client-server/simple-server.py
+++ client-server/simple-server.py
@@ -1,15 +1,18 @@
+#!/usr/bin/env python3
+
 import socket
-serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serv.bind(('0.0.0.0', 8080))
-serv.listen(5)
-while True:
-    conn, addr = serv.accept()
-    from_client = ''
-    while True:
-        data = conn.recv(4096)
-        if not data: break
-        from_client += data
-        print from_client
-        conn.send("I am SERVER<br>")
-    conn.close()
-    print 'client disconnected'
+
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
